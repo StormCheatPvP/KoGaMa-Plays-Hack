05f8907d15c64ae71eb040554d0baa29074da8e6
StormCheatPvP <114351474+StormCheatPvP@users.noreply.github.com>
2022-12-23 10:58:32 -0300

    Update

--- KoGaMaPlaysHack.user.js
+++ KoGaMaPlaysHack.user.js
@@ -6,5 +6,6 @@
 // @match                https://kogama.com.br/*
 // @match                https://friends.kogama.com/*
 // @require             https://raw.githubusercontent.com/StormCheatPvP/KoGaMa-Plays-Hack/main/CodeHack.js
+// @icon                 https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://kogama.com.br&size=64
 // @grant                none
 // ==/UserScript==
