bf912e6ee2b7e6724fb9de29152dfe12a0d765ff
StormCheatPvP <114351474+StormCheatPvP@users.noreply.github.com>
2022-12-23 10:53:12 -0300

    Script KGM Plays Hack

---
+++ KoGaMaPlaysHack.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name                 KoGaMa Plays Hack
+// @version              alpha 0.4
+// @author               Undefine
+// @match                https://www.kogama.com/*
+// @match                https://kogama.com.br/*
+// @match                https://friends.kogama.com/*
+// @requiere             https://raw.githubusercontent.com/StormCheatPvP/KoGaMa-Plays-Hack/main/CodeHack.js
+// @grant                none
+// ==/UserScript==
